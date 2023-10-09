d3a279fe74de00d1e6dc3f014ecd734b41b5d715
Kevin Nilsson <kevin7254@gmail.com>
2023-10-09 21:12:56 +0200

    Added homescreen
    Added a simple homescreen to Snake Game. Only "Play Game" is working
    at the moment.

---
+++ app/src/main/java/com/example/snakegame/HomeActivity.kt
@@ -0,0 +1,29 @@
+package com.example.snakegame
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+        findViewById<View>(R.id.btnPlayGame).setOnClickListener {
+            val gameIntent = Intent(this, MainActivity::class.java)
+            startActivity(gameIntent)
+        }
+
+        findViewById<View>(R.id.btnStats).setOnClickListener {
+            // val statsIntent = Intent(this, StatsActivity::class.java)
+            // startActivity(statsIntent)
+        }
+
+        findViewById<View>(R.id.btnSettings).setOnClickListener {
+            // val settingsIntent = Intent(this, SettingsActivity::class.java)
+            //  startActivity(settingsIntent)
+        }
+    }
+}
